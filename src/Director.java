d22a41f60f30abd34b098c70481e04c13a1be90a
vadim1-art <vadimgotovec1@gmail.com>
2026-01-17 16:44:45 +0300

    Update Director.java

--- src/Director.java
+++ src/Director.java
@@ -2,9 +2,11 @@ public class Director extends Person {
     private int numberOfShows;
 
     public Director(String name, String surname, Gender gender, int numberOfShows) {
-        super(name, surname, gender);
+        super(name, surname);
         this.numberOfShows = numberOfShows;
     }
 
-    public int getNumberOfShows() { return numberOfShows; }
+    public int getNumberOfShows() {
+        return numberOfShows;
+    }
 }
